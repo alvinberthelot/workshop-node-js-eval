a12c3025aa7aec8580eaf1d976aab2c1dfaeebdf
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2017-11-29 17:42:42 +0100

    Solution exercise 5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,10 @@
+
+const fs = require('fs');
+
 exports.run = function(input) {
   console.log("Exercise #5");
+  var data = fs.readFileSync(input, 'utf8')
+  return data.split('\n')
+    .map(item => parseInt(item))
+    .reduce((acc, item) => acc + item, 0);
 };
