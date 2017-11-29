f85a7084cc5f9805319d97f3c2d5aeede3da61b8
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2017-11-29 17:30:45 +0100

    Solution exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -2,6 +2,15 @@ exports.run = function(input) {
   console.log("Exercise #4");
 
   // YOUR CODE IS WELCOME HERE
+  var myFunction = (numbersValues, stringsValues) => {
+    var result = ""
+    var separator = ""
+    for (let i = 0; i < numbersValues.length; i++) {
+      result += `${separator}The value of ${stringsValues[i]} is ${numbersValues[i]}`;
+      separator = ", ";
+    }
+    return result;
+  }
 
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(myFunction, [1, 2, 3], ["One", "Two", "Three"]);
