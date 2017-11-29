081f9ecc1a0d9650542dfdf6dc6076e3ca1aad3d
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2017-11-29 17:24:22 +0100

    Solution exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,10 @@
 exports.run = function(input) {
   console.log("Exercise #3");
+  return input.reduce((acc, item) => {
+    if (item.operator === 'add') {
+      return acc + item.value;
+    } else {
+      return acc - item.value;
+    }
+  }, 0);
 };
